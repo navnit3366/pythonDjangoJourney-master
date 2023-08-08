16730e8754870114e8706892b366f5b1f51ae30b
Navnit P <navnit3366@gmail.com>
2023-08-08 11:23:12 +0530

    Deploying pythonDjangoJourney-master

---
+++ builtinFunctionListCompre/builtinFxnListCompre.py
@@ -0,0 +1,24 @@
+"""
+Mostly used built-in functions and List Comprehension!!!
+
+Built-in Functions
+Range function, Enumerate, zip, in
+
+List Comprehensions
+Provide a concise way to create the lists
+
+The list comprehension always returns a result list
+"""
+## Mostly used Built-in functions in the Python
+#1. Range Function
+for num in range(10):
+    print(num)      # This generates sequence of numbers in the form of list (0-9)
+
+for num in range(3,11):
+    print(num)      # This generates sequence of numbers in the form of list (3-10)
+
+for num in range(3,11,2):
+    print(num)      # This generates sequence of numbers in the form of list (3,5,7,9)
+                    # The 2 is the difference between the numbers
+
+
